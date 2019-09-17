96486bab584a6f76156fb2376af38929a701dfd0
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-09-17 10:17:27 -0700

    still working on backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, ROute, Switch } from 'react-router-dom'
+
+
+import Home from './Components/Home/Home'
+import FilteredHomes from './Components/FilteredHomes/FilteredHomes'
+// import UnfilteredHomes from 
+import Listing from './Components/Listing/Listing'
+import NewProperty from './Components/NewProperty/NewProperty'
+import NewPropConfirm from './Components/NewPropConfirm/NewPropConfirm'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/filteredHomes/:city_id" component={FilteredHomes} />
+          <Route path="/unfilteredHomes" component={UnfilteredHomes} />
+          <Route path="/listing/:property_id" component={Listing} />
+          <Route path="/newProperty" component={NewProperty} />
+          <Route path="/propertyConfirm" component={NewPropConfirm} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+import userReducer from './ducks/userReducer'
+import homesReducer from './ducks/homesReducer'
+import listingReducer from './ducks/listingReducer'
+import bookingReducer from './ducks/bookingReducer'
+import filterReducer from './ducks/filterReducer'
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    homes: homesReducer, 
+    listing: listingReducer,
+    booking: bookingReducer,
+    filtered: filterReducer
+})
+
+export default createStore(rootReducer, applyMiddleware(promiseMiddleware))
